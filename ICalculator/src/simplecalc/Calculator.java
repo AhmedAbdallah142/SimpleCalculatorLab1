75df4699eda52f41319a0b00b04d57103fdf6218
ahmed <ahmed@DESKTOP-SDPV2B4>
2020-02-19 23:35:48 +0200

    class

--- ICalculator/src/simplecalc/Calculator.java
+++ ICalculator/src/simplecalc/Calculator.java
@@ -2,11 +2,12 @@ package simplecalc;
 
 public class Calculator implements ICalculator {
 	
-	
+	@Override
 	public int add(int x ,int y) {
 		return x+y;
 	}
 	
+	@Override
 	public float divide (int x ,int y ) {
 		if (y==0) {
 			throw new RuntimeException("cannot divide by zero");
